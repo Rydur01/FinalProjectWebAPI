8dc875a16477d0b5edc033732e37c37b0a1bc992
Modupe Daniel <daniemu@mail.uc.edu>
2022-11-17 15:35:11 -0500

    Almost done

--- FinalProjectWebAPI/Controllers/HobbiesController.cs
+++ FinalProjectWebAPI/Controllers/HobbiesController.cs
@@ -3,8 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FinalProjectWebAPI.Controllers
 {
-    public class HobbiesController
-    {
+    
         [ApiController]
         [Route("[controller]")]
         public class HobbiesController : ControllerBase
@@ -42,6 +41,119 @@ namespace FinalProjectWebAPI.Controllers
                 }
 
             }
+
+        [HttpGet]
+        [Route("All")]
+        [ProducesResponseType(typeof(List<Hobbies>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                if (_context.Hobbies == null || !_context.Hobbies.Any())
+                    return NotFound("No Hobbies found in the database");
+                return Ok(_context.Hobbies?.ToList());
+            }
+            catch (Exception e)
+            {
+                return Problem(e.Message);
+            }
+
+        }
+
+        [HttpDelete]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var hobby = _context.Hobbies?.Find(id);
+                if (hobby == null)
+                {
+                    return NotFound($"Hobby with id {id} was not found");
+                }
+
+                _context.Hobbies?.Remove(hobby);
+                var result = _context.SaveChanges();
+                if (result >= 1)
+                {
+                    return Ok("Delete operation was successful");
+                }
+                return Problem("Delete was not successful. Please try again");
+            }
+            catch (Exception e)
+            {
+                return Problem(e.Message);
+            }
+
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+        public IActionResult Add(Hobbies hobbyToAdd)
+        {
+            if (hobbyToAdd.Id != 0)
+            {
+                return BadRequest("Id was provided but not needed");
+            }
+            try
+            {
+                //HobbyToAdd.Id = 0;
+                _context.Hobbies?.Add(hobbyToAdd);
+                var result = _context.SaveChanges();
+                if (result >= 1)
+                {
+                    return Ok($"Hobby {hobbyToAdd.Name} added successfully");
+                }
+                return Problem("Add was not successful. Please try again");
+            }
+            catch (Exception e)
+            {
+                return Problem(e.Message);
+            }
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+        public IActionResult Put(Hobbies hobbyToEdit)
+        {
+            if (hobbyToEdit.Id < 1)
+            {
+                return BadRequest("Please provide a valid id");
+            }
+
+            try
+            {
+                var hobby = _context.Hobbies?.Find(hobbyToEdit.Id);
+                if (hobby == null)
+                    return NotFound("The hobby was not found");
+
+                
+                hobby.Name = hobbyToEdit.Name;
+                hobby.Reason = hobbyToEdit.Reason;
+                hobby.Days = hobbyToEdit.Days;
+                hobby.Hobby = hobbyToEdit.Hobby;
+                _context.Hobbies?.Update(hobby);
+                var result = _context.SaveChanges();
+                if (result >= 1)
+                {
+                    return Ok("Hobby edited successfully");
+                }
+                return Problem("Edit was not successful. Please try again");
+            }
+            catch (Exception e)
+            {
+                return Problem(e.Message);
+            }
         }
     }
-}
+    }
+
--- FinalProjectWebAPI/Models/HobbiesContext.cs
+++ FinalProjectWebAPI/Models/HobbiesContext.cs
@@ -18,17 +18,3 @@ namespace FinalProjectWebAPI.Models
         }
     }
 }
-/**
-public ToDoContext(DbContextOptions<ToDoContext> options) : base(options)
-        {
-
-}
-
-public DbSet<ToDo>? ToDos { get; set; }
-
-protected override void OnModelCreating(ModelBuilder modelBuilder)
-{
-    modelBuilder.Entity<ToDo>().HasData(
-        new ToDo { Id = 1, DueDate = DateTime.Today, Name = "Homework" });
-}
-*/
