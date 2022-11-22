6dd3b7e78937ae4165f092456ab1234c7a24b6a6
rydur <rydur@LAPTOP-2BJS2A85>
2022-11-22 13:00:19 -0500

    added nswag, work in progress models folder

---
+++ FinalProjectWebAPI/Models/ToDo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace FinalProjectWebAPI.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
+
--- FinalProjectWebAPI/Program.cs
+++ FinalProjectWebAPI/Program.cs
@@ -1,19 +1,24 @@
+using DependencyInjectionDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ToDoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ToDoContext")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerDocument();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseOpenApi();
+    app.UseSwaggerUi3();
 }
 
 app.UseHttpsRedirection();
