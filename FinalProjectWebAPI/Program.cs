e726f63f51818a63dba27ca2f17db350488816af
Modupe Daniel <daniemu@mail.uc.edu>
2022-11-22 13:09:49 -0500

    Switched to nswag

--- FinalProjectWebAPI/Program.cs
+++ FinalProjectWebAPI/Program.cs
@@ -6,14 +6,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerDocument();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseOpenApi();
+    app.UseSwaggerUi3();
 }
 
 app.UseHttpsRedirection();
