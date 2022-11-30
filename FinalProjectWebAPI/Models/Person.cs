55596a005be45fad2a2fa4faec65fd1f8e62dba3
BurnsSurface <Burns@SURFACE>
2022-11-30 17:08:56 -0500

    finished branch

---
+++ FinalProjectWebAPI/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace FinalProjectWebAPI.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public string Program { get; set; }
+
+        public string Year { get; set; }
+    }
+}
---
+++ FinalProjectWebAPI/Program.cs
@@ -0,0 +1,31 @@
+using FinalProjectWebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<PersonContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PersonContext")));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerDocument();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUi3();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
