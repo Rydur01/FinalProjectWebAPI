d2bdc2da5fa31512e2fdbcb79c2729cfdaa29abc
rydur <rydur@LAPTOP-2BJS2A85>
2022-12-01 17:47:33 -0500

    added people table

--- FinalProjectWebAPI/Models/Person.cs
+++ FinalProjectWebAPI/Models/Person.cs
@@ -4,12 +4,12 @@
     {
         public int ID { get; set; }
 
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
         public DateTime Birthday { get; set; }
 
-        public string Program { get; set; }
+        public string? Program { get; set; }
 
-        public string Year { get; set; }
+        public string? Year { get; set; }
     }
 }
