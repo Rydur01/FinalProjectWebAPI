f5250ef2167ae6861afdb5835b0a2655e7254fe6
grantkollar <89742140+grantkollar@users.noreply.github.com>
2022-12-01 01:39:37 -0500

    setting up controller

--- FinalProjectWebAPI/Program.cs
+++ FinalProjectWebAPI/Program.cs
@@ -1,3 +1,7 @@
+using FinalProjectWebAPI.Model;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
